fbb8ddfa3e9852044482282a55293bad93065fb7
vv221 <vv221@dotslashplay.it>
2017-07-03 10:50:21 +0200

    2.0 - A Boy and His Blob - Add icons pack as an optional archive

--- play.it-2/games/play-a-boy-and-and-his-blob.sh
+++ play.it-2/games/play-a-boy-and-and-his-blob.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170616.1
+script_version=20170703.1
 
 # Set game-specific variables
 
@@ -48,6 +48,9 @@ ARCHIVE_GOG_MD5='7025963a3a26f838877374f72ce3760d'
 ARCHIVE_GOG_SIZE='1300000'
 ARCHIVE_GOG_VERSION='20160421-gog2.1.0.2'
 
+ARCHIVE_ICONS='a-boy-and-his-blob_icons.tar.gz'
+ARCHIVE_ICONS_MD5='2a555c1f6b02a45b8932c8e72a9c1dd6'
+
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*'
 
@@ -57,11 +60,14 @@ ARCHIVE_GAME_BIN_FILES='./Blob ./lib*.so.*'
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
 ARCHIVE_GAME_DATA_FILES='./content'
 
+ARCHIVE_ICONS_PATH='.'
+ARCHIVE_ICONS_FILES='./16x16 ./32x32 ./48x48 ./64x64'
+
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE='Blob'
-APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='256'
 APP_MAIN_LIBS='.'
+APP_MAIN_EXE='Blob'
+APP_MAIN_ICON_GOG='data/noarch/support/icon.png'
+APP_MAIN_ICON_GOG_RES='256'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN'
 
@@ -90,9 +96,21 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
+# Try to load icons archive
+
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ICONS_PACK' 'ARCHIVE_ICONS'
+ARCHIVE="$ARCHIVE_MAIN"
+
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+if [ "$ICONS_PACK" ]; then
+	(
+		ARCHIVE='ICONS_PACK'
+		extract_data_from "$ICONS_PACK"
+	)
+fi
 
 PKG='PKG_BIN'
 organize_data 'GAME_BIN' "$PATH_GAME"
@@ -101,10 +119,14 @@ PKG='PKG_DATA'
 organize_data 'DOC'       "$PATH_DOC"
 organize_data 'GAME_DATA' "$PATH_GAME"
 
-res="$APP_MAIN_ICON_RES"
-PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
-mkdir --parents "$PKG_DATA_PATH/$PATH_ICON"
-mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" "$PKG_DATA_PATH/$PATH_ICON/$GAME_ID.png"
+if [ "$ICONS_PACK" ]; then
+	organize_data 'ICONS' "$PATH_ICON_BASE"
+else
+	res="$APP_MAIN_ICON_GOG_RES"
+	PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
+	mkdir --parents "$PKG_DATA_PATH/$PATH_ICON"
+	mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON_GOG" "$PKG_DATA_PATH/$PATH_ICON/$GAME_ID.png"
+fi
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
