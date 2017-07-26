e241872d10db6d60a1a85e1c939defa5b705423d
vv221 <vv221@dotslashplay.it>
2017-07-26 15:45:11 +0200

    2.0 - Heroes of Might and Magic 3 - New GOG installers + Drop PKG_MUSIC package (merged into PKG_DATA)

--- play.it-2/games/play-heroes-of-might-and-magic-3.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-3.sh
@@ -34,27 +34,37 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170616.2
+script_version=20170726.1
 
 # Set game-specific variables
 
 GAME_ID='heroes-of-might-and-magic-3'
 GAME_NAME='Heroes of Might and Magic III'
 
-ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR ARCHIVE_GOG_EN_OLD ARCHIVE_GOG_FR_OLD'
 
-ARCHIVE_GOG_EN='setup_homm3_complete_2.0.0.16.exe'
-ARCHIVE_GOG_EN_MD5='263d58f8cc026dd861e9bbcadecba318'
-ARCHIVE_GOG_EN_VERSION='3.0-gog2.0.0.16'
-ARCHIVE_GOG_EN_PATCH='patch_heroes_of_might_and_magic_3_complete_2.0.1.17.exe'
-ARCHIVE_GOG_EN_PATCH_MD5='815b9c097cd57d0e269beb4cc718dad3'
-ARCHIVE_GOG_EN_PATCH_VERSION='3.0-gog2.0.1.17'
+ARCHIVE_GOG_EN='setup_homm_3_complete_4.0_(10665).exe'
+ARCHIVE_GOG_EN_MD5='0c97452fc4da4e8811173f21df873fab'
+ARCHIVE_GOG_EN_VERSION='4.0-gog10665'
 ARCHIVE_GOG_EN_SIZE='1100000'
 
-ARCHIVE_GOG_FR='setup_homm3_complete_french_2.1.0.20.exe'
-ARCHIVE_GOG_FR_MD5='ca8e4726acd7b5bc13c782d59c5a459b'
-ARCHIVE_GOG_FR_VERSION='3.0-gog2.1.0.20'
+ARCHIVE_GOG_FR='setup_homm_3_complete_french_4.0_(10665).exe'
+ARCHIVE_GOG_FR_MD5='6c3ee33a531bd0604679581ab267d8a3'
+ARCHIVE_GOG_FR_VERSION='4.0-gog10665'
 ARCHIVE_GOG_FR_SIZE='1100000'
+
+ARCHIVE_GOG_EN_OLD='setup_homm3_complete_2.0.0.16.exe'
+ARCHIVE_GOG_EN_OLD_MD5='263d58f8cc026dd861e9bbcadecba318'
+ARCHIVE_GOG_EN_OLD_VERSION='3.0-gog2.0.0.16'
+ARCHIVE_GOG_EN_OLD_PATCH='patch_heroes_of_might_and_magic_3_complete_2.0.1.17.exe'
+ARCHIVE_GOG_EN_OLD_PATCH_MD5='815b9c097cd57d0e269beb4cc718dad3'
+ARCHIVE_GOG_EN_OLD_PATCH_VERSION='3.0-gog2.0.1.17'
+ARCHIVE_GOG_EN_OLD_SIZE='1100000'
+
+ARCHIVE_GOG_FR_OLD='setup_homm3_complete_french_2.1.0.20.exe'
+ARCHIVE_GOG_FR_OLD_MD5='ca8e4726acd7b5bc13c782d59c5a459b'
+ARCHIVE_GOG_FR_OLD_VERSION='3.0-gog2.1.0.20'
+ARCHIVE_GOG_FR_OLD_SIZE='1100000'
 
 ARCHIVE_DOC1_PATH='tmp'
 ARCHIVE_DOC1_FILES='./*eula.txt'
@@ -66,10 +76,7 @@ ARCHIVE_GAME_BIN_PATH='app'
 ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./ifc20.dll ./ifc21.dll ./mcp.dll ./mp3dec.asi ./mss32.dll ./smackw32.dll'
 
 ARCHIVE_GAME_DATA_PATH='app'
-ARCHIVE_GAME_DATA_FILES='./data ./maps'
-
-ARCHIVE_GAME_MUSIC_PATH='app'
-ARCHIVE_GAME_MUSIC_FILES='./mp3'
+ARCHIVE_GAME_DATA_FILES='./data ./maps ./mp3'
 
 ARCHIVE_GAME_PATCH_PATH='tmp'
 ARCHIVE_GAME_PATCH_FILES='./heroes3.exe'
@@ -99,17 +106,15 @@ APP_EDITOR_CAMPAIGN_ICON='./h3ccmped.exe'
 APP_EDITOR_CAMPAIGN_ICON_RES='16 32 48 64'
 APP_EDITOR_CAMPAIGN_NAME="$GAME_NAME - campaign editor"
 
-PACKAGES_LIST='PKG_MUSIC PKG_DATA PKG_BIN'
-
-PKG_MUSIC_ID="${GAME_ID}-music"
-PKG_MUSIC_DESCRIPTION='music'
+PACKAGES_LIST='PKG_DATA PKG_BIN'
 
 PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_PROVIDE="${GAME_ID}-music"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, $PKG_MUSIC_ID, winetricks, wine32-development | wine32 | wine-bin | wine-i386 | wine-staging-i386, wine:amd64 | wine"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID $PKG_MUSIC_ID winetricks wine"
+PKG_BIN_DEPS_DEB="$PKG_DATA_ID, winetricks, wine32-development | wine32 | wine-bin | wine-i386 | wine-staging-i386, wine:amd64 | wine"
+PKG_BIN_DEPS_ARCH="$PKG_DATA_ID winetricks wine"
 
 # Load common functions
 
@@ -131,9 +136,10 @@ fi
 
 # Load patch if using GOG English archive
 
-if [ "$ARCHIVE" = 'ARCHIVE_GOG_EN' ]; then
-	set_archive 'PATCH_ARCHIVE' 'ARCHIVE_GOG_EN_PATCH'
-	ARCHIVE='ARCHIVE_GOG_EN'
+if [ "$ARCHIVE" = 'ARCHIVE_GOG_EN_OLD' ]; then
+	ARCHIVE_MAIN="$ARCHIVE"
+	set_archive 'PATCH_ARCHIVE' 'ARCHIVE_GOG_OLD_EN_PATCH'
+	ARCHIVE="$ARCHIVE_MAIN"
 	set_temp_directories $PACKAGES_LIST
 fi
 
@@ -150,9 +156,6 @@ fi
 PKG='PKG_BIN'
 organize_data 'GAME_BIN'   "$PATH_GAME"
 organize_data 'GAME_PATCH' "$PATH_GAME"
-
-PKG='PKG_MUSIC'
-organize_data 'GAME_MUSIC' "$PATH_GAME"
 
 PKG='PKG_DATA'
 organize_data 'DOC1'      "$PATH_DOC"
