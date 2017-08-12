bafb2a271f75c8d7f9be90fc0a3c43bae52906b6
Mopi <mopi@dotslashplay.it>
2017-08-12 12:23:36 +0200

    2.1 - To the Moon - New Game

---
+++ play.it-2/games/play-to-the-moon.sh
@@ -0,0 +1,137 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2017, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# To The Moon
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20170902.1
+
+# Set game-specific variables
+
+GAME_ID='to-the-moon'
+GAME_NAME='To The Moon'
+
+ARCHIVES_LIST='ARCHIVE_HUMBLE'
+
+ARCHIVE_HUMBLE='ToTheMoon_linux_1389114090.sh'
+ARCHIVE_HUMBLE_MD5='706a5c9467328438d412370ffb1454de'
+ARCHIVE_HUMBLE_SIZE='93000'
+ARCHIVE_HUMBLE_VERSION='1.0-humble1'
+ARCHIVE_HUMBLE_TYPE='mojosetup'
+
+ARCHIVE_DOC_DATA_PATH='data/noarch'
+ARCHIVE_DOC_DATA_FILES='./*.txt'
+
+ARCHIVE_GAME_BIN32_PATH='data/x86'
+ARCHIVE_GAME_BIN32_FILES='./*'
+
+ARCHIVE_GAME_BIN64_PATH='data/x86_64'
+ARCHIVE_GAME_BIN64_FILES='./*'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch'
+ARCHIVE_GAME_DATA_FILES='./*'
+
+APP_MAIN_TYPE='native_no-prefix'
+APP_MAIN_EXE_BIN32='ToTheMoon.bin.x86'
+APP_MAIN_EXE_BIN64='ToTheMoon.bin.x86_64'
+APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
+APP_MAIN_ICON='ToTheMoon.png'
+APP_MAIN_ICON_RES='32'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libasound2-plugins, libfreetype6, libgl1-mesa-glx | libgl1, libsdl2-2.0-0"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libgl lib32-alsa-lib lib32-freetype2 lib32-sdl2"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID libgl alsa-lib freetype2 sdl2"
+
+# Load common functions
+
+target_version='2.1'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+for PKG in $PACKAGES_LIST; do
+	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
+
+# Write launchers
+
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	write_launcher 'APP_MAIN'
+done
+
+# Build package
+
+postinst_icons_linking 'APP_MAIN'
+write_metadata 'PKG_DATA'
+rm "$postinst" "$prerm"
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+printf '\n'
+printf '32-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN32'
+printf '64-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN64'
+
+exit 0
