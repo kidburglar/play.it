b7cb7509f2b8dbcfcaf8c8231c7690a8a375e852
Mopi <mopi@dotslashplay.it>
2017-08-12 12:49:41 +0200

    2.0 - A Story About My Uncle - New Game

---
+++ play.it-2/games/play-a-story-about-my-uncle.sh
@@ -0,0 +1,155 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2017, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# A Story About My Uncle
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20170829.1
+
+# Set game-specific variables
+
+GAME_ID='a-story-about-my-uncle'
+GAME_NAME='A Story About My Uncle'
+
+ARCHIVES_LIST='ARCHIVE_HUMBLE'
+
+ARCHIVE_HUMBLE='Linux-NoDRM-ASAMU_5188.zip'
+ARCHIVE_HUMBLE_MD5='71f9f3add29a733c4a1a7d18d738d3d6'
+ARCHIVE_HUMBLE_SIZE='1300000'
+ARCHIVE_HUMBLE_VERSION='5188-humble170516'
+
+ARCHIVE_ICONS='a-story-about-my-uncle_icons.tar.gz'
+ARCHIVE_ICONS_MD5='db4eb7ab666e61ea5fc983102099ab31'
+
+ARCHIVE_GAME_BIN32_PATH='.'
+ARCHIVE_GAME_BIN32_FILES='Binaries/linux-x86 Binaries/SDL2/lib/linux-x86'
+
+ARCHIVE_GAME_BIN64_PATH='.'
+ARCHIVE_GAME_BIN64_FILES='Binaries/linux-amd64 Binaries/SDL2/lib/linux-amd64'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='./ASAMU ./Engine'
+
+ARCHIVE_ICONS_PATH='.'
+ARCHIVE_ICONS_FILES='32x32'
+
+CONFIG_DIRS='./ASAMU/Cloud ./ASAMU/Config'
+DATA_DIRS='./ASAMU/Saves'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE_BIN32='Binaries/linux-x86/ASAMU'
+APP_MAIN_EXE_BIN64='Binaries/linux-amd64/ASAMU'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libsdl2-2.0-0, libopenal1, libgl1-mesa-glx | libgl1"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-glibc lib32-gcc-libs lib32-sdl2 lib32-libopenal lib32-libgl"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID glibc gcc-libs sdl2 openal libgl"
+
+# Load common functions
+
+target_version='2.1'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Try to load icons archive
+
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ICONS_PACK' 'ARCHIVE_ICONS'
+ARCHIVE="$ARCHIVE_MAIN"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
+if [ "$ICONS_PACK" ]; then
+	(
+		ARCHIVE='ICONS_PACK'
+		extract_data_from "$ICONS_PACK"
+	)
+fi
+
+for PKG in $PACKAGES_LIST; do
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
+
+if [ "$ICONS_PACK" ]; then
+	PKG='PKG_DATA'
+	organize_data 'ICONS' "$PATH_ICON_BASE"
+fi
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	write_launcher 'APP_MAIN'
+done
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+printf '\n'
+printf '32-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN32'
+printf '64-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN64'
+
+exit 0
