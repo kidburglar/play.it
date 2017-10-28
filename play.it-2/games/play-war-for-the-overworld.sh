kidburglar/play.it

commit 94647189d628c7825d63453e9bb2636dc6970386

2.2 - War for the Overworld - New GOG installer + Use generic dependencies list + Code clean-up

play.it-2/games/play-war-for-the-overworld.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171014.1
+script_version=20171025.1
 
 # Set game-specific variables
 
@@ -43,58 +43,46 @@ GAME_NAME='War for the Overworld'
 
 ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_OLD ARCHIVE_GOG_OLDER ARCHIVE_GOG_OLDEST ARCHIVE_HUMBLE'
 
-ARCHIVE_GOG='war_for_the_overworld_en_1_6_4_15562.sh'
-ARCHIVE_GOG_MD5='45847e6faf4114e266d0fef99cae42b6'
+ARCHIVE_GOG='war_for_the_overworld_en_1_6_5f1_15803.sh'
+ARCHIVE_GOG_MD5='5fb91f8c86eafeea09c91b36c0c82afe'
 ARCHIVE_GOG_SIZE='2800000'
-ARCHIVE_GOG_VERSION='1.6.4-gog15562'
+ARCHIVE_GOG_VERSION='1.6.5f1-gog15803'
 ARCHIVE_GOG_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLD='war_for_the_overworld_en_1_6_4_15447.sh'
-ARCHIVE_GOG_OLD_MD5='09335b964b387ce911942f6c72ab3fb0'
+ARCHIVE_GOG_OLD='war_for_the_overworld_en_1_6_4_15562.sh'
+ARCHIVE_GOG_OLD_MD5='45847e6faf4114e266d0fef99cae42b6'
 ARCHIVE_GOG_OLD_SIZE='2800000'
-ARCHIVE_GOG_OLD_VERSION='1.6.4-gog15447'
+ARCHIVE_GOG_OLD_VERSION='1.6.4-gog15562'
 ARCHIVE_GOG_OLD_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLDER='war_for_the_overworld_en_1_6_3f1_14896.sh'
-ARCHIVE_GOG_OLDER_MD5='c49bb5d5f937bf7231527b9d945fb613'
-ARCHIVE_GOG_OLDER_SIZE='2500000'
-ARCHIVE_GOG_OLDER_VERSION='1.6.3f1-gog14896'
+ARCHIVE_GOG_OLDER='war_for_the_overworld_en_1_6_4_15447.sh'
+ARCHIVE_GOG_OLDER_MD5='09335b964b387ce911942f6c72ab3fb0'
+ARCHIVE_GOG_OLDER_SIZE='2800000'
+ARCHIVE_GOG_OLDER_VERSION='1.6.4-gog15447'
 ARCHIVE_GOG_OLDER_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLDEST='gog_war_for_the_overworld_2.5.0.6.sh'
-ARCHIVE_GOG_OLDEST_MD5='f003d58cea1b2c5416a4af059768d77b'
+ARCHIVE_GOG_OLDEST='war_for_the_overworld_en_1_6_3f1_14896.sh'
+ARCHIVE_GOG_OLDEST_MD5='c49bb5d5f937bf7231527b9d945fb613'
 ARCHIVE_GOG_OLDEST_SIZE='2500000'
-ARCHIVE_GOG_OLDEST_VERSION='1.6.3-gog2.5.0.6'
+ARCHIVE_GOG_OLDEST_VERSION='1.6.3f1-gog14896'
+ARCHIVE_GOG_OLDEST_TYPE='mojosetup'
 
-ARCHIVE_HUMBLE='War_for_the_Overworld_v1.5.2_-_Linux_x64.zip'
 ARCHIVE_HUMBLE_MD5='bedee8b966767cf42c55c6b883e3127c'
 ARCHIVE_HUMBLE_SIZE='2500000'
 ARCHIVE_HUMBLE_VERSION='1.5.2-humble170202'
 
 ARCHIVE_DOC_DATA_PATH_GOG='data/noarch/docs'
-ARCHIVE_DOC_DATA_PATH_GOG_OLD='data/noarch/docs'
-ARCHIVE_DOC_DATA_PATH_GOG_OLDER='data/noarch/docs'
-ARCHIVE_DOC_DATA_PATH_GOG_OLDEST='data/noarch/docs'
 ARCHIVE_DOC_DATA_FILES='./*'
 
 ARCHIVE_GAME_BIN_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_BIN_PATH_GOG_OLD='data/noarch/game'
-ARCHIVE_GAME_BIN_PATH_GOG_OLDER='data/noarch/game'
-ARCHIVE_GAME_BIN_PATH_GOG_OLDEST='data/noarch/game'
 ARCHIVE_GAME_BIN_PATH_HUMBLE='Linux'
 ARCHIVE_GAME_BIN_FILES='./*.x86_64 ./*_Data/Plugins ./*_Data/Mono ./*_Data/CoherentUI_Host'
 
 ARCHIVE_GAME_ASSETS_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_ASSETS_PATH_GOG_OLD='data/noarch/game'
-ARCHIVE_GAME_ASSETS_PATH_GOG_OLDER='data/noarch/game'
-ARCHIVE_GAME_ASSETS_PATH_GOG_OLDEST='data/noarch/game'
 ARCHIVE_GAME_ASSETS_PATH_HUMBLE='Linux'
 ARCHIVE_GAME_ASSETS_FILES='./*_Data/*.assets*'
 
 ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game'
-ARCHIVE_GAME_DATA_PATH_GOG_OLD='data/noarch/game'
-ARCHIVE_GAME_DATA_PATH_GOG_OLDER='data/noarch/game'
-ARCHIVE_GAME_DATA_PATH_GOG_OLDEST='data/noarch/game'
 ARCHIVE_GAME_DATA_PATH_HUMBLE='Linux'
 ARCHIVE_GAME_DATA_FILES='./*_Data/globalgamemanagers ./*_Data/resources.resource ./*_Data/level* ./*_Data/*.dat ./*_Data/*.ini ./*_Data/*.png ./*_Data/GameData ./*_Data/Managed ./*_Data/Resources ./*_Data/Translation ./*_Data/uiresources'
 
@@ -117,12 +105,13 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='64'
-PKG_BIN_DEPS_DEB="$PKG_ASSETS_ID, $PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1, libxcursor1"
-PKG_BIN_DEPS_ARCH="$PKG_ASSETS_ID $PKG_DATA_ID glibc gcc-libs libgl libxcursor"
+PKG_BIN_DEPS="$PKG_ASSETS_ID $PKG_DATA_ID glibc libstdc++ glx xcursor"
+PKG_BIN_DEPS_ARCH='gconf'
+PKG_BIN_DEPS_DEB='libgconf-2-4'
 
 # Load common functions
 
-target_version='2.1'
+target_version='2.2'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"

commit e928e770dab0635e730a1bcd8573bd886fc6ac37

2.2 - War for the Overworld - Fix Humble installer support broken by commit 94647189d628c7825d63453e9bb2636dc6970386

play.it-2/games/play-war-for-the-overworld.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171025.1
+script_version=20171028.1
 
 # Set game-specific variables
 
@@ -67,6 +67,7 @@ ARCHIVE_GOG_OLDEST_SIZE='2500000'
 ARCHIVE_GOG_OLDEST_VERSION='1.6.3f1-gog14896'
 ARCHIVE_GOG_OLDEST_TYPE='mojosetup'
 
+ARCHIVE_HUMBLE='War_for_the_Overworld_v1.5.2_-_Linux_x64.zip'
 ARCHIVE_HUMBLE_MD5='bedee8b966767cf42c55c6b883e3127c'
 ARCHIVE_HUMBLE_SIZE='2500000'
 ARCHIVE_HUMBLE_VERSION='1.5.2-humble170202'