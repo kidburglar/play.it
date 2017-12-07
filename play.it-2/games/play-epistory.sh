bdb13280dc100191f703462d37cb555a69508c48
Mopi <mopi@dotslashplay.it>
2017-12-07 20:46:04 +0100

    2.3 - Epistory Typing Chronicles - New game

---
+++ play.it-2/games/play-epistory.sh
@@ -0,0 +1,138 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2017, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Epistory - Typing Chronicles
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20171207.1
+
+# Set game-specific variables
+
+GAME_ID='epistory-typing-chronicles'
+GAME_NAME='Epistory - Typing Chronicles'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
+
+ARCHIVE_GOG='gog_epistory_typing_chronicles_2.2.0.3.sh'
+ARCHIVE_GOG_MD5='8db1f835a9189099e57c174ba2353f53'
+ARCHIVE_GOG_SIZE='1300000'
+ARCHIVE_GOG_VERSION='1.3.5-gog2.2.0.3'
+
+ARCHIVE_DOC_PATH='data/noarch/docs'
+ARCHIVE_DOC_FILES='./*'
+
+ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+
+ARCHIVE_GAME_BIN64_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='./*_Data'
+
+DATA_DIRS='./logs'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE_BIN32='./Epistory.x86'
+APP_MAIN_EXE_BIN64='./Epistory.x86_64'
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
+APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
+APP_MAIN_ICON='*_Data/Resources/UnityPlayer.png'
+APP_MAIN_ICON_RES='128'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc glx xcursor libxrandr"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+
+# Load common functions
+
+target_version='2.3'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+for PKG in $PACKAGES_LIST; do
+	organize_data "DOC_${PKG#PKG_}" "$PATH_DOC"
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	write_launcher 'APP_MAIN'
+done
+
+# Build package
+
+postinst_icons_linking 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg
+
+# Clean up
+
+rm --recursive "${PLAYIT_WORKDIR}"
+
+# Print instructions
+
+printf '\n'
+printf '32-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN32'
+printf '64-bit:'
+print_instructions 'PKG_DATA' 'PKG_BIN64'
+
+exit 0
