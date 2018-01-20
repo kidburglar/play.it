88d1fe26dace0ad9b5cd994508228f8f7ea32e6b
vv221 <vv221@dotslashplay.it>
2018-01-20 19:33:21 +0100

    2.5 - Faster Than Light - New GOG installer + Generic dependencies + Syntax tweaks

--- play.it-2/games/play-faster-than-light.sh
+++ play.it-2/games/play-faster-than-light.sh
@@ -34,19 +34,25 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170605.1
+script_version=20180120.1
 
 # Set game-specific variables
 
 GAME_ID='faster-than-light'
 GAME_NAME='Faster Than Light'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_HUMBLE'
+ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_OLD ARCHIVE_HUMBLE'
 
-ARCHIVE_GOG='gog_ftl_advanced_edition_2.0.0.2.sh'
-ARCHIVE_GOG_MD5='2c24b70b31316acefedc082e9441a69a'
-ARCHIVE_GOG_SIZE='220000'
-ARCHIVE_GOG_VERSION='1.5.13-gog2.0.0.2'
+ARCHIVE_GOG='ftl_advanced_edition_en_1_6_3_17917.sh'
+ARCHIVE_GOG_MD5='b64692d5302a1ab60d912c5eb5fbc5e4'
+ARCHIVE_GOG_SIZE='210000'
+ARCHIVE_GOG_VERSION='1.6.3-gog17917'
+ARCHIVE_GOG_TYPE='mojosetup'
+
+ARCHIVE_GOG_OLD='gog_ftl_advanced_edition_2.0.0.2.sh'
+ARCHIVE_GOG_OLD_MD5='2c24b70b31316acefedc082e9441a69a'
+ARCHIVE_GOG_OLD_SIZE='220000'
+ARCHIVE_GOG_OLD_VERSION='1.5.13-gog2.0.0.2'
 
 ARCHIVE_HUMBLE='FTL.1.5.13.tar.gz'
 ARCHIVE_HUMBLE_MD5='791e0bc8de73fcdcd5f461a4548ea2d8'
@@ -63,21 +69,33 @@ ARCHIVE_DOC2_FILES='./licenses'
 
 ARCHIVE_GAME_BIN32_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_BIN32_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_BIN32_FILES='./x86'
+ARCHIVE_GAME_BIN32_FILES_GOG='./FTL.x86'
+ARCHIVE_GAME_BIN32_FILES_GOG_OLD='./x86'
+ARCHIVE_GAME_BIN32_FILES_HUMBLE='./x86'
 
 ARCHIVE_GAME_BIN64_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_BIN64_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_BIN64_FILES='./amd64'
+ARCHIVE_GAME_BIN64_FILES_GOG='./FTL.amd64'
+ARCHIVE_GAME_BIN64_FILES_GOG_OLD='./amd64'
+ARCHIVE_GAME_BIN64_FILES_HUMBLE='./amd64'
 
 ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_DATA_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_DATA_FILES='./exe_icon.bmp ./resources'
+ARCHIVE_GAME_DATA_FILES_GOG='./exe_icon.bmp ./ftl.dat'
+ARCHIVE_GAME_DATA_FILES_GOG_OLD='./exe_icon.bmp ./resources'
+ARCHIVE_GAME_DATA_FILES_HUMBLE='./exe_icon.bmp ./resources'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE_BIN32='x86/bin/FTL'
-APP_MAIN_EXE_BIN64='amd64/bin/FTL'
-APP_MAIN_LIBS_BIN32='x86/lib'
-APP_MAIN_LIBS_BIN64='amd64/lib'
+APP_MAIN_EXE_BIN32_GOG='FTL.x86'
+APP_MAIN_EXE_BIN32_GOG_OLD='x86/bin/FTL'
+APP_MAIN_EXE_BIN32_HUMBLE='x86/bin/FTL'
+APP_MAIN_EXE_BIN64_GOG='FTL.amd64'
+APP_MAIN_EXE_BIN64_GOG_OLD='amd64/bin/FTL'
+APP_MAIN_EXE_BIN64_HUMBLE='amd64/bin/FTL'
+APP_MAIN_LIBS_BIN32_GOG_OLD='x86/lib'
+APP_MAIN_LIBS_BIN32_HUMBLE='x86/lib'
+APP_MAIN_LIBS_BIN64_GOG_OLD='amd64/lib'
+APP_MAIN_LIBS_BIN64_HUMBLE='amd64/lib'
 APP_MAIN_ICON1='exe_icon.bmp'
 APP_MAIN_ICON1_RES='64'
 APP_MAIN_ICON2='resources/exe_icon.bmp'
@@ -89,21 +107,23 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1, libsdl1.2debian, libglu1-mesa | libglu1"
-PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libgl lib32-sdl lib32-glu"
+PKG_BIN32_DEPS_GOG="$PKG_DATA_ID glu"
+PKG_BIN32_DEPS_GOG_OLD="$PKG_DATA_ID sdl glu"
+PKG_BIN32_DEPS_HUMBLE="$PKG_DATA_ID sdl glu"
 
 PKG_BIN64_ARCH='64'
-PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
-PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID libgl sdl glu"
+PKG_BIN64_DEPS_GOG="$PKG_BIN32_DEPS_GOG"
+PKG_BIN64_DEPS_GOG_OLD="$PKG_BIN32_DEPS_GOG_OLD"
+PKG_BIN64_DEPS_HUMBLE="$PKG_BIN32_DEPS_HUMBLE"
 
 # Load common functions
 
-target_version='2.0'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
 	elif [ -e './libplayit2.sh' ]; then
 		PLAYIT_LIB2='./libplayit2.sh'
 	else
@@ -119,16 +139,11 @@ fi
 extract_data_from "$SOURCE_ARCHIVE"
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
 
-PKG='PKG_BIN32'
-organize_data 'GAME_BIN32' "$PATH_GAME"
-
-PKG='PKG_BIN64'
-organize_data 'GAME_BIN64' "$PATH_GAME"
-
-PKG='PKG_DATA'
-organize_data 'DOC1'      "$PATH_DOC"
-organize_data 'DOC2'      "$PATH_DOC"
-organize_data 'GAME_DATA' "$PATH_GAME"
+for PKG in $PACKAGES_LIST; do
+	organize_data "DOC1_${PKG#PKG_}" "$PATH_DOC"
+	organize_data "DOC2_${PKG#PKG_}" "$PATH_DOC"
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
 
 extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON1"
 res="$APP_MAIN_ICON1_RES"
@@ -136,17 +151,21 @@ PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
 mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
 mv "$PLAYIT_WORKDIR/icons/${APP_MAIN_ICON1%.bmp}.png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
 
-extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
-res="$APP_MAIN_ICON2_RES"
-PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
-mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
-mv "$PLAYIT_WORKDIR/icons/$(basename ${APP_MAIN_ICON2%.bmp}).png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
+if [ "$ARCHIVE" = 'ARCHIVE_GOG_OLD' ] || [ "$ARCHIVE" = 'ARCHIVE_HUMBLE' ]; then
+	extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
+	res="$APP_MAIN_ICON2_RES"
+	PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
+	mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
+	mv "$PLAYIT_WORKDIR/icons/$(basename ${APP_MAIN_ICON2%.bmp}).png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
+fi
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	use_archive_specific_value "APP_MAIN_EXE_${PKG#PKG_}"
+	use_archive_specific_value "APP_MAIN_LIBS_${PKG#PKG_}"
 	write_launcher 'APP_MAIN'
 done
 
