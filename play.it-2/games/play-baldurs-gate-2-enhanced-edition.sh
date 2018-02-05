kidburglar/play.it

commit 577279209efbc88739aff0f50ebf917ce2cccd31

2.5 - Fix all scripts using libssl 1.0.0 to pass syntax checks added in version 2.5.0

play.it-2/games/play-baldurs-gate-2-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180102.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -48,8 +48,8 @@ ARCHIVE_GOG_MD5='b9ee856a29238d4aec65367377d88ac4'
 ARCHIVE_GOG_SIZE='2700000'
 ARCHIVE_GOG_VERSION='2.3.67.3-gog2.6.0.11'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
 ARCHIVE_DOC_DATA_FILES='./*'
@@ -81,7 +81,7 @@ PKG_BIN_DEPS="$PKG_AREAS_ID $PKG_L10N_ID $PKG_DATA_ID glibc libstdc++ glx openal
 
 # Load common functions
 
-target_version='2.4'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -99,8 +99,9 @@ fi
 
 # Use libSSL 1.0.0 32-bit archive
 
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
-ARCHIVE='ARCHIVE_GOG'
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
+ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
 

play.it-2/games/play-baldurs-gate-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180102.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -53,8 +53,8 @@ ARCHIVE_GOG_OLD_MD5='37ece59534ca63a06f4c047d64b82df9'
 ARCHIVE_GOG_OLD_SIZE='3200000'
 ARCHIVE_GOG_OLD_VERSION='2.3.67.3-gog2.5.0.7'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
 ARCHIVE_DOC_DATA_FILES='./*'
@@ -92,7 +92,7 @@ PKG_BIN_DEPS="$PKG_AREAS_ID $PKG_L10N_ID $PKG_DATA_ID glibc libstdc++ glx openal
 
 # Load common functions
 
-target_version='2.4'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -110,8 +110,9 @@ fi
 
 # Use libSSL 1.0.0 32-bit archive
 
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
-ARCHIVE='ARCHIVE_GOG'
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
+ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
 

play.it-2/games/play-fran-bow.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171115.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -48,8 +48,8 @@ ARCHIVE_GOG_MD5='6e3013e9c8be4d25e1815f00bc177941'
 ARCHIVE_GOG_SIZE='530000'
 ARCHIVE_GOG_VERSION='160315-gog2.3.0.5'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*'
@@ -77,7 +77,7 @@ PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-glu lib32-openal lib32-libxrandr"
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -95,8 +95,9 @@ fi
 
 # Use libSSL 1.0.0 32-bit archive
 
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
-ARCHIVE='ARCHIVE_GOG'
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
+ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
 

play.it-2/games/play-icewind-dale-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171228.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -48,8 +48,8 @@ ARCHIVE_GOG_MD5='fc7244f4793eec365b8ac41d91a4edbb'
 ARCHIVE_GOG_SIZE='2900000'
 ARCHIVE_GOG_VERSION='1.4.0-gog2.1.0.5'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_ICONS='icewind-dale-enhanced-edition_icons.tar.gz'
 ARCHIVE_ICONS_MD5='afe7a2a8013a859f7b56a3104eacd783'
@@ -88,7 +88,7 @@ PKG_BIN_DEPS_ARCH="$PKG_L10N_ID $PKG_DATA_ID lib32-libgl lib32-openal lib32-json
 
 # Load common functions
 
-target_version='2.4'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -113,7 +113,7 @@ ARCHIVE="$ARCHIVE_MAIN"
 # Use libSSL 1.0.0 32-bit archive
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
 ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data

play.it-2/games/play-risk-of-rain.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171225.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -53,8 +53,8 @@ ARCHIVE_HUMBLE_MD5='21eb80a7b517d302478c4f86dd5ea9a2'
 ARCHIVE_HUMBLE_SIZE='100000'
 ARCHIVE_HUMBLE_VERSION='1.3.0-humble160519'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_DOC_PATH_GOG='data/noarch/docs'
 ARCHIVE_DOC_FILES_GOG='./*'
@@ -85,7 +85,7 @@ PKG_BIN_DEPS_DEB='libcurl3'
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -104,7 +104,7 @@ fi
 # Use libSSL 1.0.0 32-bit archive
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
 ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data

play.it-2/games/play-undertale.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171128.1
+script_version=20180130.1
 
 # Set game-specific variables
 
@@ -54,8 +54,8 @@ ARCHIVE_GOG_OLD_MD5='e740df4e15974ad8c21f45ebe8426fb0'
 ARCHIVE_GOG_OLD_SIZE='160000'
 ARCHIVE_GOG_OLD_VERSION='1.001-gog2.0.0.1'
 
-ARCHIVE_LIBSSL='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
 
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*'
@@ -83,7 +83,7 @@ PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glu openal libxrandr"
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -102,7 +102,7 @@ fi
 # Use libSSL 1.0.0 32-bit archive
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'LIBSSL' 'ARCHIVE_LIBSSL'
+set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_32'
 ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data

commit 3e54467ded8c1fc7838bb8a14dd6a8e49c4bfc62

2.5 - Fix all scripts using libssl 1.0.0 32-bit archive, broken by commit 577279209efbc88739aff0f50ebf917ce2cccd31

play.it-2/games/play-baldurs-gate-2-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.1
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -119,10 +119,12 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
+	(
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
+	)
 	dir='libs'
-	ARCHIVE='LIBSSL'
-	extract_data_from "$LIBSSL"
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	APP_MAIN_LIBS="$dir"

play.it-2/games/play-baldurs-gate-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.1
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -130,10 +130,12 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
+	(
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
+	)
 	dir='libs'
-	ARCHIVE='LIBSSL'
-	extract_data_from "$LIBSSL"
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	APP_MAIN_LIBS="$dir"

play.it-2/games/play-fran-bow.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.1
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -114,10 +114,12 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
+	(
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
+	)
 	dir='libs'
-	ARCHIVE='LIBSSL'
-	extract_data_from "$LIBSSL"
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	APP_MAIN_LIBS="$dir"

play.it-2/games/play-icewind-dale-enhanced-edition.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.2
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -147,10 +147,12 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
+	(
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
+	)
 	dir='libs'
-	ARCHIVE='LIBSSL'
-	extract_data_from "$LIBSSL"
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}/$dir"
 	APP_MAIN_LIBS="$dir"

play.it-2/games/play-risk-of-rain.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.1
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -120,10 +120,10 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
 	(
-		ARCHIVE='LIBSSL'
-		extract_data_from "$LIBSSL"
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
 	)
 	dir='libs'
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"

play.it-2/games/play-undertale.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180130.1
+script_version=20180205.1
 
 # Set game-specific variables
 
@@ -118,10 +118,10 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-if [ "$LIBSSL" ]; then
+if [ "$ARCHIVE_LIBSSL" ]; then
 	(
-		ARCHIVE='LIBSSL'
-		extract_data_from "$LIBSSL"
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
 	)
 	dir='libs'
 	mkdir --parents "${PKG_BIN_PATH}${PATH_GAME}/$dir"