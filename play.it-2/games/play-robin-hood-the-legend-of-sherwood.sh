12b3b8ab46015dc220068ae9152e100aff61bd2e
vv221 <vv221@dotslashplay.it>
2018-02-11 00:30:33 +0100

    2.5 - Robin Hood: The Legend of Sherwood - New archives + Update binary used to run the game + Update old archives version number + Drop unused files

--- play.it-2/games/play-robin-hood-the-legend-of-sherwood.sh
+++ play.it-2/games/play-robin-hood-the-legend-of-sherwood.sh
@@ -34,42 +34,46 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20171227.1
+script_version=20180211.1
 
 # Set game-specific variables
 
 GAME_ID='robin-hood-the-legend-of-sherwood'
 GAME_NAME='Robin Hood: The Legend of Sherwood'
 
-ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_EN_OLD ARCHIVE_GOG_FR ARCHIVE_GOG_FR_OLD'
 
-ARCHIVE_GOG_EN='setup_robin_hood_2.0.0.12.exe'
-ARCHIVE_GOG_EN_MD5='9e2452c88f154c5e0306ca98e6b773ef'
-ARCHIVE_GOG_EN_VERSION='1.0-gog2.0.0.12'
-ARCHIVE_GOG_EN_SIZE='1100000'
+ARCHIVE_GOG_EN='setup_robin_hood_-_the_legend_of_sherwood_1.1_(17797).exe'
+ARCHIVE_GOG_EN_MD5='e8808cdafc7ea75cbcfaa850275b3dd6'
+ARCHIVE_GOG_EN_VERSION='1.1-gog17797'
+ARCHIVE_GOG_EN_SIZE='1200000'
 
-ARCHIVE_GOG_FR='setup_robin_hood_french_2.1.0.15.exe'
-ARCHIVE_GOG_FR_MD5='f6775cefa54e15141b855d037eafb8d9'
-ARCHIVE_GOG_FR_VERSION='1.0-gog2.1.0.15'
-ARCHIVE_GOG_FR_SIZE='1100000'
+ARCHIVE_GOG_EN_OLD='setup_robin_hood_2.0.0.12.exe'
+ARCHIVE_GOG_EN_OLD_MD5='9e2452c88f154c5e0306ca98e6b773ef'
+ARCHIVE_GOG_EN_OLD_VERSION='1.1-gog2.0.0.12'
+ARCHIVE_GOG_EN_OLD_SIZE='1100000'
+
+ARCHIVE_GOG_FR='setup_robin_hood_-_the_legend_of_sherwood_french_1.1_(17797).exe'
+ARCHIVE_GOG_FR_MD5='8b19812fb424651fc482cb7a9c5ed665'
+ARCHIVE_GOG_FR_VERSION='1.1-gog17797'
+ARCHIVE_GOG_FR_SIZE='1200000'
+
+ARCHIVE_GOG_FR_OLD='setup_robin_hood_french_2.1.0.15.exe'
+ARCHIVE_GOG_FR_OLD_MD5='f6775cefa54e15141b855d037eafb8d9'
+ARCHIVE_GOG_FR_OLD_VERSION='1.1-gog2.1.0.15'
+ARCHIVE_GOG_FR_OLD_SIZE='1100000'
 
 ARCHIVE_DOC_L10N_PATH='app'
 ARCHIVE_DOC_L10N_FILES='./manual.pdf'
 
 ARCHIVE_GAME_BIN_PATH='app'
-ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./fmod.dll ./msvc?90.dll ./sdl.dll ./launch/settings.ini ./data/savegame.exe'
+ARCHIVE_GAME_BIN_FILES='./robin?hood.exe ./binkw32.dll ./fmod.dll ./msvc?90.dll ./sdl.dll ./launch/settings.ini ./data/savegame.exe'
 
-ARCHIVE_GAME1_L10N_PATH='app'
-ARCHIVE_GAME1_L10N_FILES='./campaign.bck ./1036 ./2047 ./data/sounds/fx_0017.sfk ./data/sounds/snd_055.sfk ./data/configuration/profile.cpf ./data/interface ./splash/microids_1024x768.bmp ./data/text/rhlevelsc.red ./data/levels'
+ARCHIVE_GAME_L10N_PATH='app'
+ARCHIVE_GAME_L10N_FILES='./game.exe ./1036 ./2047 ./data/sounds/fx_0017.sfk ./data/sounds/snd_055.sfk ./data/configuration/profile.cpf ./data/interface ./splash/microids_1024x768.bmp ./data/text/rhlevelsc.red ./data/levels'
 
-ARCHIVE_GAME2_L10N_PATH='app/__support/save'
-ARCHIVE_GAME2_L10N_FILES='./campaign.bck'
-
-ARCHIVE_GAME1_DATA_PATH='app'
-ARCHIVE_GAME1_DATA_FILES='./data/robinhood.bks ./data/levels/attack ./data/levels/custom? ./data/levels/day ./data/levels/fog ./data/levels/night ./data/savegame ./data/sounds/fx_0019.sfk ./data/configuration/keyset?.cfg ./data/configuration/release.log ./splash/config.txt ./data/text/actors.res ./data/text/rhlevela?.red ./data/text/rhleveld?.red ./data/text/rhlevele?.red ./data/text/rhlevelh?.red ./data/text/rhlevelsa.red ./data/text/rhlevelsb.red ./data/text/rhlevelsd.red ./data/text/rhlevelse.red ./data/text/rhlevelt?.red ./data/text/rhlevelv?.red ./data/sounds/*.wav ./data/sounds/exclamations ./data/sounds/menu ./data/sounds/robin?hood.fxg ./data/animations ./data/characters ./data/musics ./data/robinhood.dic'
-
-ARCHIVE_GAME2_DATA_PATH='app/__support/save'
-ARCHIVE_GAME2_DATA_FILES='./data/savegame'
+ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_FILES='./data/robinhood.bks ./data/levels/attack ./data/levels/custom? ./data/levels/day ./data/levels/fog ./data/levels/night ./data/sounds/fx_0019.sfk ./data/configuration/keyset?.cfg ./data/configuration/release.log ./splash/config.txt ./data/text/actors.res ./data/text/rhlevela?.red ./data/text/rhleveld?.red ./data/text/rhlevele?.red ./data/text/rhlevelh?.red ./data/text/rhlevelsa.red ./data/text/rhlevelsb.red ./data/text/rhlevelsd.red ./data/text/rhlevelse.red ./data/text/rhlevelt?.red ./data/text/rhlevelv?.red ./data/sounds/*.wav ./data/sounds/exclamations ./data/sounds/menu ./data/sounds/robin?hood.fxg ./data/animations ./data/characters ./data/musics ./data/robinhood.dic'
 
 CONFIG_FILES='./launch/settings.ini'
 CONFIG_DIRS='./data/configuration'
@@ -77,7 +81,7 @@ DATA_DIRS='./data/savegame'
 DATA_FILES='./campaign.bck'
 
 APP_MAIN_TYPE='wine'
-APP_MAIN_EXE='robin hood.exe'
+APP_MAIN_EXE='game.exe'
 APP_MAIN_ICON='robin hood.exe'
 APP_MAIN_ICON_RES='16 32'
 
@@ -98,7 +102,7 @@ PKG_BIN_DEPS="$PKG_L10N_ID $PKG_DATA_ID wine"
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.5'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -122,8 +126,6 @@ tolower "$PLAYIT_WORKDIR/gamedata"
 for PKG in $PACKAGES_LIST; do
 	organize_data "DOC_${PKG#PKG_}"   "$PATH_DOC"
 	organize_data "GAME_${PKG#PKG_}"  "$PATH_GAME"
-	organize_data "GAME1_${PKG#PKG_}" "$PATH_GAME"
-	organize_data "GAME2_${PKG#PKG_}" "$PATH_GAME"
 done
 
 PKG='PKG_BIN'
