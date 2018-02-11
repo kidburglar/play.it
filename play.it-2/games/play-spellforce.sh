e589119c1170c4af84608eb23f4affe031bbfacc
Mopi <mopi@dotslashplay.it>
2018-02-11 17:49:27 +0100

    2.5 - Spellforce - New game

---
+++ play.it-2/games/play-spellforce.sh
@@ -0,0 +1,145 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Spellforce
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180217.1
+
+# Set game-specific variables
+
+GAME_ID='spellforce'
+GAME_NAME='Spellforce'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
+
+ARCHIVE_GOG='setup_spellforce_-_platinum_edition_1.54.75000_(17748).exe'
+ARCHIVE_GOG_MD5='ed34fb43d8042ff61a889865148b8dd2'
+ARCHIVE_GOG_SIZE='3900000'
+ARCHIVE_GOG_VERSION='1.54.7500-gog17748'
+ARCHIVE_GOG_TYPE='innosetup'
+ARCHIVE_GOG_PART1='setup_spellforce_-_platinum_edition_1.54.75000_(17748)-1.bin'
+ARCHIVE_GOG_PART1_MD5='60fca014e7ccd4630a7ec3cedb23942a'
+ARCHIVE_GOG_PART1_TYPE='innosetup'
+
+ARCHIVE_DOC_DATA_PATH='app'
+ARCHIVE_DOC_DATA_FILES='./manual.pdf ./readme.rtf'
+
+ARCHIVE_GAME_BIN_PATH='app'
+ARCHIVE_GAME_BIN_FILES='./ar.exe ./binkw32.dll ./dbghelp.dll ./mss32.dll ./spellforce.exe'
+
+ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_FILES='./data ./map ./miles ./pak ./videos ./spellforce_addon2.ico ./spellforce_addon.ico'
+
+APP_REGEDIT='cdkey.reg'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='spellforce.exe'
+APP_MAIN_ICON='spellforce.exe'
+APP_MAIN_ICON_RES='16 32 48'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID wine"
+
+# Load common functions
+
+target_version='2.5'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Check that all parts of the installer are present
+
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ARCHIVE_PART1' "${ARCHIVE_MAIN}_PART1"
+[ "$ARCHIVE_PART1" ] || set_archive_error_not_found "${ARCHIVE_MAIN}_PART1"
+ARCHIVE="$ARCHIVE_MAIN"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+for PKG in $PACKAGES_LIST; do
+	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
+
+PKG='PKG_BIN'
+extract_and_sort_icons_from 'APP_MAIN'
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Register CD key
+
+cat > "${PKG_BIN_PATH}${PATH_GAME}/cdkey.reg" << 'EOF'
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall\SpellForce]
+"CDKEY"="2M0VY-3QIPS-TTKC1-RV90B-0QMXG"
+EOF
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
