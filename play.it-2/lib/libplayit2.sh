0f55017be3a8e70c461258b5b33de665ce2f948a
vv221 <vv221@dotslashplay.it>
2018-02-11 13:10:08 +0100

    2.5.1 release

--- play.it-2/lib/libplayit2.sh
+++ play.it-2/lib/libplayit2.sh
@@ -32,8 +32,8 @@
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-library_version=2.5.1~dev
-library_revision=20180211.3
+library_version=2.5.1
+library_revision=20180211.4
 
 # set package distribution-specific architecture
 # USAGE: set_architecture $pkg
