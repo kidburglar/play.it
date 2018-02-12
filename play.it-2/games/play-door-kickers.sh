835b8df0a28acdd64b79583734935540808687a5
mortalius <mortalius@gmail.com>
2018-02-12 20:36:29 +0100

    2.5 - Door Kickers - New game

---
+++ play.it-2/games/play-door-kickers.sh
@@ -0,0 +1,134 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Door Kickers
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180217.1
+
+# Set game-specific variables
+
+GAME_ID='door-kickers'
+GAME_NAME='Door Kickers'
+
+ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_HUMBLE'
+
+ARCHIVE_GOG='gog_door_kickers_2.7.0.11.sh'
+ARCHIVE_GOG_MD5='29efa58e4a61060b0b1211dddd2476a1'
+ARCHIVE_GOG_SIZE='1400000'
+ARCHIVE_GOG_VERSION='1.0.9-gog2.7.0.11'
+
+ARCHIVE_HUMBLE='DoorKickers1448920440.tar.gz'
+ARCHIVE_HUMBLE_MD5='0126db31867ae0e7a7eceee54de4a177'
+ARCHIVE_HUMBLE_SIZE='1400000'
+ARCHIVE_HUMBLE_VERSION='1.0.9-humble151130'
+
+ARCHIVE_DOC_DATA_PATH_GOG='data/noarch/docs'
+ARCHIVE_DOC_DATA_PATH_HUMBLE='DoorKickers'
+ARCHIVE_DOC_DATA_FILES_GOG='./*'
+ARCHIVE_DOC_DATA_FILES_HUMBLE='./*.txt'
+
+ARCHIVE_GAME_BIN_PATH_GOG='data/noarch/game'
+ARCHIVE_GAME_BIN_PATH_HUMBLE='DoorKickers'
+ARCHIVE_GAME_BIN_FILES='./DoorKickers'
+
+ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game'
+ARCHIVE_GAME_DATA_PATH_HUMBLE='DoorKickers'
+ARCHIVE_GAME_DATA_FILES='./data ./linux_libs ./mods'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='DoorKickers'
+APP_MAIN_ICON='data/noarch/support/icon.png'
+APP_MAIN_ICON_RES='256'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glu openal sdl2"
+
+# Load common functions
+
+target_version='2.5'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+for PKG in $PACKAGES_LIST; do
+	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
+	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
+done
+
+if [ -f "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" ]; then
+	PKG='PKG_DATA'
+	get_icon_from_temp_dir 'APP_MAIN'
+fi
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
