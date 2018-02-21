98e1bc719ddb1b5f8121d7d4cedd22cccb0ea7e9
vv221 <vv221@dotslashplay.it>
2018-02-21 20:57:04 +0100

    2.5 - Binaries - Add dependency on PulseAudio

--- play.it-2/games/play-binaries.sh
+++ play.it-2/games/play-binaries.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180211.1
+script_version=20180221.1
 
 # Set game-specific variables
 
@@ -60,6 +60,7 @@ ARCHIVE_GAME_DATA_FILES='./*_Data/globalgamemanagers ./*_Data/*.assets ./*_Data/
 DATA_DIRS='./logs'
 
 APP_MAIN_TYPE='native'
+APP_MAIN_PRERUN='pulseaudio --start'
 APP_MAIN_EXE_BIN64='Binaries.x86'
 APP_MAIN_EXE_BIN64='Binaries.x86_64'
 APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
@@ -73,7 +74,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr pulseaudio"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
