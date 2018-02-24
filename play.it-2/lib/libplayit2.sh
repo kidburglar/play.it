2f69df2b459e2bcb07ddefbbeee313df3d70e8c7
vv221 <vv221@dotslashplay.it>
2018-02-24 21:54:36 +0100

    2.6 library - Add automatic architecture handling to print_instructions

--- play.it-2/lib/libplayit2.sh
+++ play.it-2/lib/libplayit2.sh
@@ -33,7 +33,7 @@
 ###
 
 library_version=2.6.0~dev
-library_revision=20180304.1
+library_revision=20180304.2
 
 # set package distribution-specific architecture
 # USAGE: set_architecture $pkg
@@ -1471,7 +1471,26 @@ print_instructions() {
 		print_instructions $PACKAGES_LIST
 		return 0
 	fi
+	local package_arch
+	local packages_list_32
+	local packages_list_64
+	local packages_list_all
 	local string
+	for package in "$@"; do
+		package_arch="$(eval printf -- '%b' \"\$${package}_ARCH\")"
+		case "$package_arch" in
+			('32')
+				packages_list_32="$packages_list_32 $package"
+			;;
+			('64')
+				packages_list_64="$packages_list_64 $package"
+			;;
+			(*)
+				packages_list_all="$packages_list_all $package"
+			;;
+		esac
+
+	done
 	case "${LANG%_*}" in
 		('fr')
 			string='\nInstallez %s en lançant la série de commandes suivantes en root :\n'
@@ -1481,6 +1500,39 @@ print_instructions() {
 		;;
 	esac
 	printf "$string" "$GAME_NAME"
+	if [ -n "$packages_list_32" ] && [ -n "$packages_list_64" ]; then
+		print_instructions_architecture_specific '32' $packages_list_all $packages_list_32
+		print_instructions_architecture_specific '64' $packages_list_all $packages_list_64
+	else
+		case $OPTION_PACKAGE in
+			('arch')
+				print_instructions_arch "$@"
+			;;
+			('deb')
+				print_instructions_deb "$@"
+			;;
+			(*)
+				liberror 'OPTION_PACKAGE' 'print_instructions'
+			;;
+		esac
+	fi
+	printf '\n'
+}
+
+# print installation instructions for Arch Linux - 32-bit version
+# USAGE: print_instructions_architecture_specific $pkg[…]
+# CALLS: print_instructions_arch print_instructions_deb
+print_instructions_architecture_specific() {
+	case "${LANG%_*}" in
+		('fr')
+			string='\nversion %s-bit :\n'
+		;;
+		('en'|*)
+			string='\n%s-bit version:\n'
+		;;
+	esac
+	printf "$string" "$1"
+	shift 1
 	case $OPTION_PACKAGE in
 		('arch')
 			print_instructions_arch "$@"
@@ -1492,7 +1544,6 @@ print_instructions() {
 			liberror 'OPTION_PACKAGE' 'print_instructions'
 		;;
 	esac
-	printf '\n'
 }
 
 # print installation instructions for Arch Linux
--- play.it-2/src/30_installation-instructions.sh
+++ play.it-2/src/30_installation-instructions.sh
@@ -7,7 +7,26 @@ print_instructions() {
 		print_instructions $PACKAGES_LIST
 		return 0
 	fi
+	local package_arch
+	local packages_list_32
+	local packages_list_64
+	local packages_list_all
 	local string
+	for package in "$@"; do
+		package_arch="$(eval printf -- '%b' \"\$${package}_ARCH\")"
+		case "$package_arch" in
+			('32')
+				packages_list_32="$packages_list_32 $package"
+			;;
+			('64')
+				packages_list_64="$packages_list_64 $package"
+			;;
+			(*)
+				packages_list_all="$packages_list_all $package"
+			;;
+		esac
+
+	done
 	case "${LANG%_*}" in
 		('fr')
 			string='\nInstallez %s en lançant la série de commandes suivantes en root :\n'
@@ -17,6 +36,39 @@ print_instructions() {
 		;;
 	esac
 	printf "$string" "$GAME_NAME"
+	if [ -n "$packages_list_32" ] && [ -n "$packages_list_64" ]; then
+		print_instructions_architecture_specific '32' $packages_list_all $packages_list_32
+		print_instructions_architecture_specific '64' $packages_list_all $packages_list_64
+	else
+		case $OPTION_PACKAGE in
+			('arch')
+				print_instructions_arch "$@"
+			;;
+			('deb')
+				print_instructions_deb "$@"
+			;;
+			(*)
+				liberror 'OPTION_PACKAGE' 'print_instructions'
+			;;
+		esac
+	fi
+	printf '\n'
+}
+
+# print installation instructions for Arch Linux - 32-bit version
+# USAGE: print_instructions_architecture_specific $pkg[…]
+# CALLS: print_instructions_arch print_instructions_deb
+print_instructions_architecture_specific() {
+	case "${LANG%_*}" in
+		('fr')
+			string='\nversion %s-bit :\n'
+		;;
+		('en'|*)
+			string='\n%s-bit version:\n'
+		;;
+	esac
+	printf "$string" "$1"
+	shift 1
 	case $OPTION_PACKAGE in
 		('arch')
 			print_instructions_arch "$@"
@@ -28,6 +80,5 @@ print_instructions() {
 			liberror 'OPTION_PACKAGE' 'print_instructions'
 		;;
 	esac
-	printf '\n'
 }
 
