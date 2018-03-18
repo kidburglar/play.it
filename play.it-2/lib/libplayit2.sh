809d3f889b9845ef72c526ff83352b7086383874
vv221 <vv221@dotslashplay.it>
2018-03-18 20:35:20 +0100

    2.7.2 release

--- play.it-2/lib/libplayit2.sh
+++ play.it-2/lib/libplayit2.sh
@@ -32,8 +32,8 @@
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-library_version=2.7.2~dev
-library_revision=20180318.8
+library_version=2.7.2
+library_revision=20180318.9
 
 # set package distribution-specific architecture
 # USAGE: set_architecture $pkg
--- play.it-2/src/00_header.sh
+++ play.it-2/src/00_header.sh
@@ -32,6 +32,6 @@
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-library_version=2.7.2~dev
-library_revision=20180318.8
+library_version=2.7.2
+library_revision=20180318.9
 
