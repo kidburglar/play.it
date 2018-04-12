7b3f567ade183abe7cb718cadee5186c163dc527
vv221 <vv221@dotslashplay.it>
2018-04-12 21:07:21 +0200

    2.7 - The Westport Independent - Add optional icons pack

--- play.it-2/games/play-the-westport-independent.sh
+++ play.it-2/games/play-the-westport-independent.sh
@@ -34,20 +34,22 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20180413.1
 
 # Set game-specific variables
 
 GAME_ID='the-westport-independent'
 GAME_NAME='The Westport Independent'
 
-ARCHIVES_LIST='ARCHIVE_GOG'
-
 ARCHIVE_GOG='gog_the_westport_independent_2.0.0.1.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/westport_independent_the'
 ARCHIVE_GOG_MD5='7032059f085e94f52444e9bf4ed0195c'
 ARCHIVE_GOG_SIZE='130000'
 ARCHIVE_GOG_VERSION='1.0.0-gog2.0.0.1'
+
+ARCHIVE_EXTRA_ICONS='the-westport-independent_icons.tar.gz'
+ARCHIVE_EXTRA_ICONS_URL='https://www.dotslashplay.it/ressources/the-westport-independant/'
+ARCHIVE_EXTRA_ICONS_MD5='054b51fe4c02f256b8130b40e93d28ae'
 
 ARCHIVE_DOC1_DATA_PATH='data/noarch/docs'
 ARCHIVE_DOC1_DATA_FILES='./*'
@@ -63,6 +65,9 @@ ARCHIVE_GAME_BIN64_FILES='./the_westport_independent'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game/linux32'
 ARCHIVE_GAME_DATA_FILES='./config.json ./assets'
+
+ARCHIVE_ICONS_PATH='.'
+ARCHIVE_ICONS_FILES='./*'
 
 CONFIG_FILES='./config.json'
 
@@ -84,7 +89,7 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.7'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
@@ -100,19 +105,30 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
+# Try to load icons archive
+
+ARCHIVE_MAIN="$ARCHIVE"
+set_archive 'ARCHIVE_ICONS' 'ARCHIVE_EXTRA_ICONS'
+ARCHIVE="$ARCHIVE_MAIN"
+
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC1_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "DOC2_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Extract game icon
 
 PKG='PKG_DATA'
-get_icon_from_temp_dir 'APP_MAIN'
-
+if [ "$ARCHIVE_ICONS" ]; then
+	rm --recursive "$PLAYIT_WORKDIR/gamedata"
+	(
+		ARCHIVE='ARCHIVE_ICONS'
+		extract_data_from "$ARCHIVE_ICONS"
+	)
+	organize_data 'ICONS' "$PATH_ICON_BASE"
+else
+	get_icon_from_temp_dir 'APP_MAIN'
+fi
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
@@ -132,10 +148,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
