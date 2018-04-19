325eb3417f70ecac2b5efe21a8d2c86fb5479deb
Mopi <mopi@dotslashplay.it>
2018-04-19 15:29:12 +0200

    2.7 - Superhot - New script

---
+++ play.it-2/games/play-superhot.sh
@@ -0,0 +1,117 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Superhot
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180419.1
+
+# Set game-specific variables
+
+GAME_ID='superhot'
+GAME_NAME='Superhot'
+
+ARCHIVE_HUMBLE='SUPERHOT_LINUX.zip'
+ARCHIVE_HUMBLE_MD5='bbbbae191504b00cfb4a9509175014c2'
+ARCHIVE_HUMBLE_VERSION='1.0-humble1'
+ARCHIVE_HUMBLE_SIZE='4300000'
+
+ARCHIVE_GAME_BIN_PATH='SUPERHOT'
+ARCHIVE_GAME_BIN_FILES='./SUPERHOT.x86_64 *_Data/*/x86_64'
+
+ARCHIVE_GAME_DATA_PATH='SUPERHOT'
+ARCHIVE_GAME_DATA_FILES='*_Data/globalgamemanagers *_Data/*.assets *_Data/level* *_Data/*.resS *_Data/ScreenSelector.png *_Data/*.resource *_Data/Managed *_Data/Mono/etc *_Data/Resources *_Data/StreamingAssets'
+
+DATA_DIRS='./logs'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='SUPERHOT.x86_64'
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
+APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
+APP_MAIN_ICON='SUPERHOT_Data/Resources/UnityPlayer.png'
+APP_MAIN_ICON_RES='128'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='64'
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libgl xcursor libxrandr"
+
+# Load common functions
+
+target_version='2.7'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+postinst_icons_linking 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN'
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
