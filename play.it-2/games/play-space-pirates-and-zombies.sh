983ae06a23894c9364445671bfdbae4b8262f526
vv221 <vv221@dotslashplay.it>
2018-05-06 14:22:19 +0200

    2.8 - Space Pirates and Zombies - New game

---
+++ play.it-2/games/play-space-pirates-and-zombies.sh
@@ -0,0 +1,119 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Space Pirates and Zombies
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180506.1
+
+# Set game-specific variables
+
+GAME_ID='space-pirates-and-zombies'
+GAME_NAME='Space Pirates and Zombies'
+
+ARCHIVE_GOG='gog_space_pirates_and_zombies_2.0.0.4.sh'
+ARCHIVE_GOG_URL='https://www.gog.com/game/space_pirates_and_zombies'
+ARCHIVE_GOG_MD5='46da2a84e78f8016e35f7c0e63e28581'
+ARCHIVE_GOG_VERSION='1.605-gog2.0.0.4'
+ARCHIVE_GOG_SIZE='180000'
+
+ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC0_DATA_FILES='./*'
+
+ARCHIVE_DOC1_DATA_PATH='data/noarch/game'
+ARCHIVE_DOC1_DATA_FILES='./README-linux.txt'
+
+ARCHIVE_GAME_BIN_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN_FILES='./SPAZ ./audio.so'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='./common ./game ./icon.bmp ./mods ./SPAZ.png'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='SPAZ'
+APP_MAIN_ICON='SPAZ.png'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ sdl1.2 openal"
+
+# Load common functions
+
+target_version='2.8'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+PKG='PKG_DATA'
+icons_linking_postinst 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN'
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
