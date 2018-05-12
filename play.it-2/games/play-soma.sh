27ed3dd5d1004ea002ed2724ee8c8d5f523b52e6
vv221 <vv221@dotslashplay.it>
2018-05-12 19:40:18 +0200

    2.8 - SOMA - New archive + Syntax updates

--- play.it-2/games/play-soma.sh
+++ play.it-2/games/play-soma.sh
@@ -34,33 +34,44 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180415.2
+script_version=20180512.2
 
 # Set game-specific variables
 
 GAME_ID='soma'
 GAME_NAME='SOMA'
 
-ARCHIVE_HUMBLE='SOMA_Humble_Linux_1109.zip'
+ARCHIVE_HUMBLE='SOMA_Linux_v110.zip'
 ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/soma'
-ARCHIVE_HUMBLE_MD5='63f4c611fed4df25bee3fb89177ab57f'
-ARCHIVE_HUMBLE_VERSION='1109-humble1'
+ARCHIVE_HUMBLE_MD5='46e9dadf90d347e0f384e636e71ce746'
+ARCHIVE_HUMBLE_VERSION='1.10-humble2'
 ARCHIVE_HUMBLE_SIZE='22000000'
 ARCHIVE_HUMBLE_TYPE='zip'
 
-ARCHIVE_DOC_DATA_PATH='Linux'
+ARCHIVE_HUMBLE_OLD='SOMA_Humble_Linux_1109.zip'
+ARCHIVE_HUMBLE_OLD_MD5='63f4c611fed4df25bee3fb89177ab57f'
+ARCHIVE_HUMBLE_OLD_VERSION='1109-humble1'
+ARCHIVE_HUMBLE_OLD_SIZE='22000000'
+ARCHIVE_HUMBLE_OLD_TYPE='zip'
+
+ARCHIVE_DOC_DATA_PATH='SOMA'
+ARCHIVE_DOC_DATA_PATH_HUMBLE_OLD='Linux'
 ARCHIVE_DOC_DATA_FILES='./README.linux'
 
-ARCHIVE_GAME_BIN_PATH='Linux'
+ARCHIVE_GAME_BIN_PATH='SOMA'
+ARCHIVE_GAME_BIN_PATH_HUMBLE_OLD='Linux'
 ARCHIVE_GAME_BIN_FILES='./Soma.bin.x86_64 ./lib64'
 
-ARCHIVE_GAME_DATA_PATH='Linux'
+ARCHIVE_GAME_DATA_PATH='SOMA'
+ARCHIVE_GAME_DATA_PATH_HUMBLE_OLD='Linux'
 ARCHIVE_GAME_DATA_FILES='./billboards ./combos ./config ./core ./detail_meshes ./fonts ./graphics ./gui ./hps_api.hps ./hps_syntax.xml ./hps.xml ./Icon.bmp ./lang ./lights ./MainEditorSettings.cfg ./maps ./materials.cfg ./music ./particles ./resources.cfg ./script ./_shadersource ./_shadersource ./static_objects ./_supersecret.rar ./terminals ./textures ./undergrowth'
 
-ARCHIVE_GAME_ENTITIES_PATH='Linux'
+ARCHIVE_GAME_ENTITIES_PATH='SOMA'
+ARCHIVE_GAME_ENTITIES_PATH_HUMBLE_OLD='Linux'
 ARCHIVE_GAME_ENTITIES_FILES='./entities'
 
-ARCHIVE_GAME_SOUNDS_PATH='Linux'
+ARCHIVE_GAME_SOUNDS_PATH='SOMA'
+ARCHIVE_GAME_SOUNDS_PATH_HUMBLE_OLD='Linux'
 ARCHIVE_GAME_SOUNDS_FILES='./sounds'
 
 CONFIG_FILES='./*.cfg'
@@ -68,11 +79,9 @@ CONFIG_DIRS='./config'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='Soma.bin.x86_64'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
-APP_MAIN_ICON='./Icon.bmp'
-APP_MAIN_ICON_RES='96'
+APP_MAIN_ICON='Icon.bmp'
 
-PACKAGES_LIST='PKG_BIN PKG_DATA PKG_ENTITIES PKG_SOUNDS'
+PACKAGES_LIST='PKG_BIN PKG_ENTITIES PKG_SOUNDS PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
@@ -88,15 +97,25 @@ PKG_BIN_DEPS="$PKG_DATA_ID $PKG_ENTITIES_ID $PKG_SOUNDS_ID glu sdl2"
 
 # Load common functions
 
-target_version='2.7'
+target_version='2.8'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
 		printf '\n\033[1;31mError:\033[0m\n'
 		printf 'libplayit2.sh not found.\n'
 		return 1
@@ -108,18 +127,19 @@ fi
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Extract icons
 
 PKG='PKG_DATA'
-extract_and_sort_icons_from 'APP_MAIN'
-
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+icons_get_from_package 'APP_MAIN'
 
 # Write launchers
 
 PKG='PKG_BIN'
 write_launcher 'APP_MAIN'
 
-# Build package
+# Build packages
 
 write_metadata
 build_pkg
