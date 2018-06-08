1575c20e29df99cbc53f42a135d145becd8e659d
Janeene Beeforth <gh@dawnmist.net>
2018-06-09 04:59:01 +1000

    Add script for Surviving Mars (GOG).

---
+++ play.it-2/games/play-surviving-mars.sh
@@ -0,0 +1,174 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Janeene Beeforth
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Surviving Mars
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180616.1
+
+# Set game-specific variables
+
+GAME_ID='surviving-mars'
+GAME_NAME='Surviving Mars'
+
+ARCHIVE_GOG='surviving_mars_en_curiosity_update_21183.sh'
+ARCHIVE_GOG_URL='https://www.gog.com/game/surviving_mars'
+ARCHIVE_GOG_MD5='ab9a61d04a128f19bc9e003214fe39a9'
+ARCHIVE_GOG_VERSION='231.139'
+ARCHIVE_GOG_TYPE='mojosetup_unzip'
+ARCHIVE_GOG_SIZE='3950000'
+
+ARCHIVE_GOG_DELUXE_UPGRADE='surviving_mars_digital_deluxe_edition_upgrade_pack_en_180423_opportunity_rc1_20289.sh'
+ARCHIVE_GOG_DELUXE_UPGRADE_MD5='a574de12f4b7f3aa1f285167109bb6a3'
+ARCHIVE_GOG_DELUXE_UPGRADE_TYPE='mojosetup_unzip'
+
+ARCHIVE_LIBSSL_64='libssl_1.0.0_64-bit.tar.gz'
+ARCHIVE_LIBSSL_64_MD5='89917bef5dd34a2865cb63c2287e0bd4'
+
+ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC_DATA_FILES='./*'
+
+ARCHIVE_GAME_BIN_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN_FILES='./MarsGOG ./libopenal.so.1 ./libSDL2-2.0.so.0 ./libpops_api.so'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='./DLC ./Licenses ./Local ./ModTools ./Movies ./Packs ./ShaderPreprocessorTemp'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='MarsGOG'
+APP_MAIN_ICON='data/noarch/support/icon.png'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN'
+
+PKG_BIN_ARCH='64'
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx"
+PKG_BIN_DEPS_ARCH='openssl-1.0'
+
+# Load common functions
+
+target_version='2.9'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Use libSSL 1.0.0 archives
+
+if [ "$OPTION_PACKAGE" != 'arch' ]; then
+	ARCHIVE_MAIN="$ARCHIVE"
+	set_archive 'ARCHIVE_LIBSSL' 'ARCHIVE_LIBSSL_64'
+	ARCHIVE="$ARCHIVE_MAIN"
+fi
+
+# Use Digital Deluxe upgrade
+
+ARCHIVE_MAIN="$ARCHIVE"
+archive_set 'ARCHIVE_DELUXE' 'ARCHIVE_GOG_DELUXE_UPGRADE'
+ARCHIVE="$ARCHIVE_MAIN"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Get icon
+
+PKG='PKG_DATA'
+icons_get_from_workdir 'APP_MAIN'
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Include libSSL into the game directory
+
+if [ "$ARCHIVE_LIBSSL" ]; then
+	(
+		ARCHIVE='ARCHIVE_LIBSSL'
+		extract_data_from "$ARCHIVE_LIBSSL"
+	)
+	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN_PATH}${PATH_GAME}"
+	rm --recursive "$PLAYIT_WORKDIR/gamedata"
+fi
+
+# Include the Digital Deluxe upgrade
+
+if [ "$ARCHIVE_DELUXE" ]; then
+	(
+		ARCHIVE='ARCHIVE_DELUXE'
+		extract_data_from "$ARCHIVE_DELUXE"
+	)
+	mv "$PLAYIT_WORKDIR/gamedata/data/noarch/docs"/* "${PKG_DATA_PATH}/${PATH_DOC}"
+	mv "$PLAYIT_WORKDIR/gamedata/data/noarch/game"/* "${PKG_DATA_PATH}/${PATH_GAME}"
+	rm --recursive "$PLAYIT_WORKDIR/gamedata"
+fi
+
+# Write launchers
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
