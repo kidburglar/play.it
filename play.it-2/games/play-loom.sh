1e143353881ef266e663bbfe44c041b97002f5cc
Mopi <mopi@dotslashplay.it>
2018-06-11 13:37:24 +0200

    2.9 - Loom - New script

---
+++ play.it-2/games/play-loom.sh
@@ -0,0 +1,122 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Solène Huault
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Loom
+# build native Linux packages from the original installers
+# send your bug reports to mopi@dotslashplay.it
+###
+
+script_version=20180611.1
+
+# Set game-specific variables
+
+GAME_ID='loom'
+GAME_NAME='Loom'
+
+ARCHIVE_GOG='loom_en_gog_2_20099.sh'
+ARCHIVE_GOG_URL='https://www.gog.com/game/loom'
+ARCHIVE_GOG_MD5='225706d082ebf907170ca899fe1dbe1c'
+ARCHIVE_GOG_SIZE='390000'
+ARCHIVE_GOG_VERSION='2-gog20099'
+ARCHIVE_GOG_TYPE='mojosetup'
+
+ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC_DATA_FILES='./*'
+
+ARCHIVE_GAME_MAIN_PATH='data/noarch/data'
+ARCHIVE_GAME_MAIN_FILES='./000.LFL ./901.LFL ./902.LFL ./903.LFL ./904.LFL ./CDDA.SOU ./DISK01.LEC ./_other'
+
+APP_MAIN_TYPE='scummvm'
+APP_MAIN_SCUMMID='loom'
+APP_MAIN_ICON='data/noarch/support/icon.png'
+
+PACKAGES_LIST='PKG_MAIN'
+
+PKG_MAIN_DEPS='scummvm'
+
+# Load common functions
+
+target_version='2.9'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Get icons
+
+icons_get_from_workdir 'APP_MAIN'
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
