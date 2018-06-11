51e3c22ff73d3ef911d1d6d42e54128ab694f7ce
Mopi <mopi@dotslashplay.it>
2018-06-11 13:34:20 +0200

    2.9 - Broken Sword II : The Smoking Mirror - Remastered - New script

---
+++ play.it-2/games/play-broken-sword-2-remastered.sh
@@ -0,0 +1,130 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Solène Huault
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Broken Sword II: The Smoking Mirror - Remastered
+# build native Linux packages from the original installers
+# send your bug reports to mopi@dotslashplay.it
+###
+
+script_version=20180611.1
+
+# Set game-specific variables
+
+GAME_ID='broken-sword-2-remastered'
+GAME_NAME='Broken Sword II: The Smoking Mirror - Remastered'
+
+ARCHIVE_GOG='gog_broken_sword_2_remastered_2.0.0.3.sh'
+ARCHIVE_GOG_URL='https://www.gog.com/game/broken_sword_2__the_smoking_mirror'
+ARCHIVE_GOG_MD5='a67ce1d6a47b74281b6f230365fb296a'
+ARCHIVE_GOG_SIZE='1400000'
+ARCHIVE_GOG_VERSION='1.0.800-gog2.0.0.3'
+
+ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC_DATA_FILES='./*'
+
+ARCHIVE_GAME_BIN_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN_FILES='./BS2Remastered_i386 ./libopenal.so.1 ./libSDL-1.2.so.0'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='./bs2.ico ./Carib1.clu ./Carib2.clu ./Carib3.clu ./Docks.clu ./FACE_L.BUK ./FACE_L.DAT ./FACE_L.DIC ./FACE_M.BUK ./FACE_M.DAT ./FACE_M.DIC ./FACE_S.BUK ./FACE_S.DAT ./FACE_S.DIC ./Fonts ./General.clu ./icon.bmp ./Jungle.clu ./menu_gfx.dat ./menu_sfx.dat ./Music.clu ./Paris.clu ./PC ./Players.clu ./Pyramid1.clu ./Pyramid2.clu ./Quaramon.clu ./Scripts.clu ./Text ./TEXTURE.DAT ./TEXTURE.DIC ./Warehous.clu'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE='BS2Remastered_i386'
+APP_MAIN_ICON='icon.bmp'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc sdl openal"
+
+# Load common functions
+
+target_version='2.9'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Extract icons
+
+PKG='PKG_DATA'
+icons_get_from_package 'APP_MAIN'
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
