9c5ac679770607314401584bf8a261bf82f5564a
vv221 <vv221@dotslashplay.it>
2018-06-19 18:56:40 +0200

    2.9 - Indiana Jones and the Fate of Atlantis - New archives

--- play.it-2/games/play-indiana-jones-and-the-fate-of-atlantis.sh
+++ play.it-2/games/play-indiana-jones-and-the-fate-of-atlantis.sh
@@ -34,37 +34,48 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20180619.1
 
 # Set game-specific variables
 
 GAME_ID='indiana-jones-and-the-fate-of-atlantis'
 GAME_NAME='Indiana Jones and the Fate of Atlantis'
 
-ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_EN_OLD ARCHIVE_GOG_FR ARCHIVE_GOG_FR_OLD'
 
-ARCHIVE_GOG_EN='gog_indiana_jones_and_the_fate_of_atlantis_2.2.0.27.sh'
+ARCHIVE_GOG_EN='indiana_jones_and_the_fate_of_atlantis_en_gog_2_20145.sh'
 ARCHIVE_GOG_EN_URL='https://www.gog.com/game/indiana_jones_and_the_fate_of_atlantis'
-ARCHIVE_GOG_EN_MD5='d56a5850b9d1d4f82eebc282ef7fb52b'
-ARCHIVE_GOG_EN_SIZE='250000'
-ARCHIVE_GOG_EN_VERSION='1.0-gog2.2.0.27'
+ARCHIVE_GOG_EN_MD5='f7c093e148b001520809032095d7e8d7'
+ARCHIVE_GOG_EN_SIZE='390000'
+ARCHIVE_GOG_EN_VERSION='1.0-gog20145'
+ARCHIVE_GOG_EN_TYPE='mojosetup'
 
-ARCHIVE_GOG_FR='gog_indiana_jones_and_the_fate_of_atlantis_french_2.2.0.27.sh'
+ARCHIVE_GOG_EN_OLD='gog_indiana_jones_and_the_fate_of_atlantis_2.2.0.27.sh'
+ARCHIVE_GOG_EN_OLD_MD5='d56a5850b9d1d4f82eebc282ef7fb52b'
+ARCHIVE_GOG_EN_OLD_SIZE='250000'
+ARCHIVE_GOG_EN_OLD_VERSION='1.0-gog2.2.0.27'
+
+ARCHIVE_GOG_FR='indiana_jones_and_the_fate_of_atlantis_fr_gog_2_20145.sh'
 ARCHIVE_GOG_FR_URL='https://www.gog.com/game/indiana_jones_and_the_fate_of_atlantis'
-ARCHIVE_GOG_FR_MD5='0da3a8b3d0505c86a9f863c27debbca7'
-ARCHIVE_GOG_FR_SIZE='99000'
-ARCHIVE_GOG_FR_VERSION='1.0-gog2.2.0.27'
+ARCHIVE_GOG_FR_MD5='edbfebcf9daa23e329e41ccf9c89286c'
+ARCHIVE_GOG_FR_SIZE='240000'
+ARCHIVE_GOG_FR_VERSION='1.0-gog20145'
+ARCHIVE_GOG_FR_TYPE='mojosetup'
 
-ARCHIVE_DOC_PATH='data/noarch/docs'
-ARCHIVE_DOC_FILES='./*.pdf ./*.txt'
+ARCHIVE_GOG_FR_OLD='gog_indiana_jones_and_the_fate_of_atlantis_french_2.2.0.27.sh'
+ARCHIVE_GOG_FR_OLD_MD5='0da3a8b3d0505c86a9f863c27debbca7'
+ARCHIVE_GOG_FR_OLD_SIZE='99000'
+ARCHIVE_GOG_FR_OLD_VERSION='1.0-gog2.2.0.27'
 
-ARCHIVE_GAME_PATH='data/noarch/data'
-ARCHIVE_GAME_FILES='./atlantis.000 ./atlantis.001 ./monster.sou'
+ARCHIVE_DOC_MAIN_PATH='data/noarch/docs'
+ARCHIVE_DOC_MAIN_FILES='./*.pdf ./*.txt'
+
+ARCHIVE_GAME_MAIN_PATH='data/noarch/data'
+ARCHIVE_GAME_MAIN_FILES='./atlantis.000 ./atlantis.001 ./monster.sou'
 
 APP_MAIN_TYPE='scummvm'
 APP_MAIN_SCUMMID='atlantis'
 APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='256x256'
 
 PACKAGES_LIST='PKG_MAIN'
 
@@ -77,15 +88,25 @@ PKG_MAIN_DEPS_ARCH='scummvm'
 
 # Load common functions
 
-target_version='2.0'
+target_version='2.9'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
 		printf '\n\033[1;31mError:\033[0m\n'
 		printf 'libplayit2.sh not found.\n'
 		exit 1
@@ -93,20 +114,15 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-
 # Extract data from game
 
 extract_data_from "$SOURCE_ARCHIVE"
 tolower "$PLAYIT_WORKDIR/gamedata"
+prepare_package_layout
 
-organize_data 'DOC'  "$PATH_DOC"
-organize_data 'GAME' "$PATH_GAME"
+# Get icon
 
-PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-
-mkdir --parents "$PKG_MAIN_PATH/$PATH_ICON"
-mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" "$PKG_MAIN_PATH/$PATH_ICON/$GAME_ID.png"
-
+icons_get_from_workdir 'APP_MAIN'
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
